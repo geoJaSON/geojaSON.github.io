8ae2915877d9c9f64f1ba644e0bb770df8950db3
Jason Jordan <jasonjordan00@gmail.com>
2023-09-15 09:39:42 -0500

    modded call method

---
+++ BRMS/data/lowprod.js
@@ -0,0 +1,128 @@
+export const lowProdData = (missionAssignmentStartDate) => [
+    [missionAssignmentStartDate + (6 * 24 * 3600 * 1000),
+        0
+    ],
+    [missionAssignmentStartDate + (7 * 24 * 3600 * 1000),
+        0
+    ],
+    [missionAssignmentStartDate + (8 * 24 * 3600 * 1000),
+        0
+    ],
+    [missionAssignmentStartDate + (9 * 24 * 3600 * 1000),
+        0
+    ],
+    [missionAssignmentStartDate + (10 * 24 * 3600 * 1000),
+        0.5
+    ],
+    [missionAssignmentStartDate + (11 * 24 * 3600 * 1000),
+        1
+    ],
+    [missionAssignmentStartDate + (12 * 24 * 3600 * 1000),
+        2
+    ],
+    [missionAssignmentStartDate + (13 * 24 * 3600 * 1000),
+        3
+    ],
+    [missionAssignmentStartDate + (14 * 24 * 3600 * 1000),
+        5
+    ],
+    [missionAssignmentStartDate + (15 * 24 * 3600 * 1000),
+        9
+    ],
+    [missionAssignmentStartDate + (16 * 24 * 3600 * 1000),
+        15
+    ],
+    [missionAssignmentStartDate + (17 * 24 * 3600 * 1000),
+        23
+    ],
+    [missionAssignmentStartDate + (18 * 24 * 3600 * 1000),
+        33
+    ],
+    [missionAssignmentStartDate + (19 * 24 * 3600 * 1000),
+        43
+    ],
+    [missionAssignmentStartDate + (20 * 24 * 3600 * 1000),
+        53
+    ],
+    [missionAssignmentStartDate + (21 * 24 * 3600 * 1000),
+        63
+    ],
+    [missionAssignmentStartDate + (22 * 24 * 3600 * 1000),
+        73
+    ],
+    [missionAssignmentStartDate + (23 * 24 * 3600 * 1000),
+        83
+    ],
+    [missionAssignmentStartDate + (24 * 24 * 3600 * 1000),
+        93
+    ],
+    [missionAssignmentStartDate + (25 * 24 * 3600 * 1000),
+        103
+    ],
+    [missionAssignmentStartDate + (26 * 24 * 3600 * 1000),
+        113
+    ],
+    [missionAssignmentStartDate + (27 * 24 * 3600 * 1000),
+        123
+    ],
+    [missionAssignmentStartDate + (28 * 24 * 3600 * 1000),
+        133
+    ],
+    [missionAssignmentStartDate + (29 * 24 * 3600 * 1000),
+        143
+    ],
+    [missionAssignmentStartDate + (30 * 24 * 3600 * 1000),
+        153
+    ],
+    [missionAssignmentStartDate + (31 * 24 * 3600 * 1000),
+        163
+    ],
+    [missionAssignmentStartDate + (32 * 24 * 3600 * 1000),
+        173
+    ],
+    [missionAssignmentStartDate + (33 * 24 * 3600 * 1000),
+        183
+    ],
+    [missionAssignmentStartDate + (34 * 24 * 3600 * 1000),
+        193
+    ],
+    [missionAssignmentStartDate + (35 * 24 * 3600 * 1000),
+        203
+    ],
+    [missionAssignmentStartDate + (36 * 24 * 3600 * 1000),
+        213
+    ],
+    [missionAssignmentStartDate + (37 * 24 * 3600 * 1000),
+        223
+    ],
+    [missionAssignmentStartDate + (38 * 24 * 3600 * 1000),
+        233
+    ],
+    [missionAssignmentStartDate + (39 * 24 * 3600 * 1000),
+        239
+    ],
+    [missionAssignmentStartDate + (40 * 24 * 3600 * 1000),
+        243
+    ],
+    [missionAssignmentStartDate + (41 * 24 * 3600 * 1000),
+        247
+    ],
+    [missionAssignmentStartDate + (42 * 24 * 3600 * 1000),
+        249
+    ],
+    [missionAssignmentStartDate + (43 * 24 * 3600 * 1000),
+        251
+    ],
+    [missionAssignmentStartDate + (44 * 24 * 3600 * 1000),
+        251
+    ],
+    [missionAssignmentStartDate + (45 * 24 * 3600 * 1000),
+        251
+    ],
+    [missionAssignmentStartDate + (46 * 24 * 3600 * 1000),
+        251
+    ],
+    [missionAssignmentStartDate + (47 * 24 * 3600 * 1000),
+        251
+    ]
+]
